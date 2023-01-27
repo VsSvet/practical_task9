01207694aed5e9db10e82ee816aaa6279a62baf2
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-28 02:08:36 +0300

    add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите первый элемент: ");
+int number1 = int.Parse(ReadLine());
+Write("Введите последний элемент: ");
+int number2 = int.Parse(ReadLine());
+
+for(int i = number1; i <= number2; i++)
+{
+    Write($"{i}, ");
+}
