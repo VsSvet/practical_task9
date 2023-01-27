0b7b2320788ae07dff2643853d8738110b3f9af5
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-28 02:13:48 +0300

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите первый элемент: ");
+int number1 = int.Parse(ReadLine());
+Write("Введите последний элемент: ");
+int number2 = int.Parse(ReadLine());
+int sum = 0;
+
+for (int i = number1; i <= number2; i++)
+{
+    sum += i;
+}
+
+WriteLine($"Сумма натуральных элементов в промежутке от {number1} до {number2} = {sum}");
