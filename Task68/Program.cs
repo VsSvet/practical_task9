c97d69d4ebb300ab9e5eb2503949622e5331a215
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-28 02:35:19 +0300

    add Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите первый элемент: ");
+int number1 = int.Parse(ReadLine());
+Write("Введите второй элемент: ");
+int number2 = int.Parse(ReadLine());
+
+WriteLine(AckermanFunction(number1, number2));
+
+
+int AckermanFunction(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AckermanFunction(m - 1, 1);
+    else
+      return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
